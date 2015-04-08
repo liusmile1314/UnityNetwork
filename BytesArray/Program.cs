0fee33758af116d5ab49b947ece31c4cca046dc7
liusmile1314 <liusmile1314@outlook.com>
2015-04-08 19:05:37 +0800

    Change

---
+++ BytesArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BytesArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data1 = { 0x12, 0x13, 0x14 };
+            byte[] data2 = { 0x14, 0x15, 0x16 };
+            byte[] data3 = new byte[data1.Length + data2.Length];
+
+            Stream s = new MemoryStream();
+            s.Write(data1, 0, data1.Length);
+        }
+    }
+}
