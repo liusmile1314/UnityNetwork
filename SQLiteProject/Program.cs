3b7cf133f0a5210c9ccbc43daf17599654102c2c
liusmile1314 <liusmile1314@outlook.com>
2015-04-14 14:40:57 +0800

    2015年4月14日14:40:54

--- SQLiteProject/Program.cs
+++ SQLiteProject/Program.cs
@@ -12,6 +12,7 @@ namespace SQLiteProject
     {
         /*SQLite是一款非常轻量级的关系数据库系统。 SQLite的主要应用场景有作为手机应用的数据库以及小型桌面软件的数据库。
          * 
+         *  Reference:http://www.cnblogs.com/teroy/p/3960760.html
          */
 
         //static readonly string DB_PATH = @"Data Source = G:\20150414\SQLiteSpy_1.9.8\Information.db3";
@@ -26,7 +27,7 @@ namespace SQLiteProject
             {
                 con.Open();
 
-                string sqlStr = @"INSERT INTO hero VALUES(3,'Love')";
+                string sqlStr = @"INSERT INTO hero VALUES(4,'End')";
                 
                 using (SQLiteCommand cmd = new SQLiteCommand(sqlStr,con))
                 {
@@ -34,11 +35,72 @@ namespace SQLiteProject
                 }
             }
         }
+
+        static void Select()
+        {
+            using (SQLiteConnection con = new SQLiteConnection(DB_PATH))
+            {
+                con.Open();
+
+                string sqlStr = @"SELECT * FROM hero";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(sqlStr, con))
+                {
+                    using (SQLiteDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            Console.WriteLine("\t{0}\t{1}",dr["hero_id"].ToString(), dr["hero_name"]);
+                        }
+                    }
+
+                    Console.WriteLine("----------------------------------");
+                }
+            }
+        }
+
+        static void Update()
+        {
+            using (SQLiteConnection con = new SQLiteConnection(DB_PATH))
+            {
+                con.Open();
+
+                string sqlStr = @"UPDATE hero SET Hero_name = 'HappyEnd' WHERE hero_id = 4";
+
+                using(SQLiteCommand cmd = new SQLiteCommand(sqlStr,con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void Delete()
+        {
+            using (SQLiteConnection con = new SQLiteConnection(DB_PATH))
+            {
+                con.Open();
+
+                string sqlStr = "DELETE FROM hero WHERE hero_id = 4";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(sqlStr, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Path : " + Environment.CurrentDirectory);
 
+            Delete();
+                Select();
+
             Insert();
+                Select();
+
+            Update();
+                Select();
 
             Console.ReadLine();
         }
